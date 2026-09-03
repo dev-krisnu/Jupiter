b7ef464704ee51a60d67afbafd5711bc0890c5a1
KRRISH JESWAR <krrishjeswar01@gmail.com>
2026-09-04 01:07:27 +0530

    initialized the files

---
+++ src/test/java/dev/krisnu/Jupiter/JupiterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.krisnu.Jupiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JupiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
